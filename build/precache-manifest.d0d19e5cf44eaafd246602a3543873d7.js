4df9ffe6df75b16eda1dd49d2abafc0b7338fb48
Melissa White <melissawhite176@gmail.com>
2020-06-24 19:31:08 -0700

    create production build and add static middleware

---
+++ build/precache-manifest.d0d19e5cf44eaafd246602a3543873d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f7c340ea83fa855c6b88b89c649ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f207ce6fabf05084f031",
+    "url": "/static/css/main.bc7f8746.chunk.css"
+  },
+  {
+    "revision": "7cee364908b574bffa22",
+    "url": "/static/js/2.1d38a834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d38a834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f207ce6fabf05084f031",
+    "url": "/static/js/main.555ca97c.chunk.js"
+  },
+  {
+    "revision": "700c674e5282f79f804a",
+    "url": "/static/js/runtime-main.ae5c0632.js"
+  }
+]);
