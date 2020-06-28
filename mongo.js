df0aa8ba937cb979330c17f6fc72590b68f38a57
Melissa White <melissawhite176@gmail.com>
2020-06-28 13:01:34 -0700

    add mongo.js and install mongoose

---
+++ mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+mongoose.connect(`mongodb+srv://fullstack:${password}@cluster0-klm2a.mongodb.net/note-app?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Another Note!',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
