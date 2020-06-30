aa0ffb8eea34cb21193528d834ee9526f1e3923c
Melissa White <melissawhite176@gmail.com>
2020-06-30 11:25:24 -0700

    add find method that searches notes stored in database (notes app - part 3c)

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -10,20 +10,29 @@ const password = process.argv[2]
 mongoose.connect(`mongodb+srv://fullstack:${password}@cluster0-klm2a.mongodb.net/note-app?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+    content: String,
+    date: Date,
+    important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'Another Note!',
-  date: new Date(),
-  important: true,
-})
+//**GENERATE NEW NOTES**
+// const note = new Note({
+//   content: 'Another Note!',
+//   date: new Date(),
+//   important: true,
+// })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+//**FIND NOTES**
+Note.find({ important: true }).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
 })
