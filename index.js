22b7c1dd7da1e5c2b6e20ff811e54fa617cac2b8
Melissa White <melissawhite176@gmail.com>
2020-07-06 18:30:10 -0700

    remove old handlers and remove generic logger - notes (part 3)

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Note = require('./models/note')
 
 app.use(express.static('build'))
 app.use(express.json())
-app.use(logger)
 app.use(cors())
 
 //create new note
@@ -35,6 +34,7 @@ const unknownEndpoint = (request, response) => {
 //handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -43,29 +43,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 app.use(requestLogger)
-
-
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -77,7 +54,6 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
 })
-
 
 //fetch individual note
 app.get('/api/notes/:id', (request, response, next) => {
@@ -105,52 +81,6 @@ const errorHandler = (error, request, response, next) => {
 
 //handler of requests with result to errors
 app.use(errorHandler)
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
