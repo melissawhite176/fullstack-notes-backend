a3fe42abd93d1d1da61fd1bb1dd09e82869f6cc5
Melissa White <melissawhite176@gmail.com>
2020-07-30 11:07:28 -0700

    update app.js with comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//app.js creates the actual application and connects to the database
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
@@ -7,6 +8,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+//-----CONNECTION TO MONGODB------------
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,15 +18,22 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   .catch((error) => {
     logger.error('error connection to MongoDB:', error.message)
   })
-
+//---------------------------------------
+//cross origin resource sharing
 app.use(cors())
+//built-in middleware to show static content
 app.use(express.static('build'))
+//json parser middleware
 app.use(express.json())
+//request logger middleware
 app.use(middleware.requestLogger)
 
+//defining router for blogs route (controllers/blogs.js)
 app.use('/api/notes', notesRouter)
 
+//unknown endpoint middleware
 app.use(middleware.unknownEndpoint)
+//error handler middleware
 app.use(middleware.errorHandler)
 
 module.exports = app
