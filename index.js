7468587af4ab90908fd520675819cfff8cf163b5
Melissa White <melissawhite176@gmail.com>
2020-07-30 11:08:28 -0700

    simplify index.js

--- index.js
+++ index.js
@@ -1,122 +1,17 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Note = require('./models/note')
+//index.js file imports the actual application from the app.js file
+//and then starts the application
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
+//contents of file used to start the application
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-//create new note
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
+//create server with Node.js HTTP module
+const server = http.createServer(app)
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-
-  note
-    .save()
-    .then(savedNote => {
-      return savedNote.toJSON()
-    })
-    .then(savedAndFormattedNote => {
-      response.json(savedAndFormattedNote)
-    })
-    .catch(error => next(error))
+//tell server to listen on the defined port (defined in config)
+//and print to console to let us know the server is up and running
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-//delete note
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-
-//toggle importance of note
-app.put('/api/notes/:id', (request, response, next) => {
-  console.log('we are doing a put')
-
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      console.log('response:', updatedNote)
-      response.json(updatedNote)
-    })
-    .catch(error => {
-      console.log('error:', error)
-      next(error)
-    })
-})
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-//new event handler to fetch notes
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-//fetch individual note
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-//error handler
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.this.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-//handler of requests with result to errors
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-//handler of requests with unknown endpoint
-app.use(unknownEndpoint)
