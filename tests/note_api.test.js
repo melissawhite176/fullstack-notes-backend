222a8ad8e8ea121ff842ac1ae4e3570bda83e547
Melissa White <melissawhite176@gmail.com>
2020-08-06 12:27:48 -0700

    create new test file (test/note_api.test) using supertest, add 'notes are returned as json' test (4b- supertest)

---
+++ tests/note_api.test.js
@@ -0,0 +1,32 @@
+/*The test imports the Express application from the app.js module
+and wraps it with the supertest function into a so-called superagent object.
+This object is assigned to the api variable and
+tests can use it for making HTTP requests to the backend.
+Our test makes an HTTP GET request to the api/notes url
+and verifies that the request is responded to with the status code 200.
+The test also verifies that the Content-Type header is set to application/json,
+indicating that the data is in the desired format.*/
+
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api= supertest(app)
+
+/*The async/await syntax is related to the fact that
+making a request to the API is an asynchronous operation.
+The Async/await syntax can be used for writing
+asynchronous code with the appearance of synchronous code.*/
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+/*Once all the tests (there is currently only one) have finished running
+we have to close the database connection used by Mongoose.
+This can be easily achieved with the afterAll method */
+afterAll(() => {
+  mongoose.connection.close()
+})
